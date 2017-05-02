49010d633f444e7d11867e498f0af27b4698a8b2
Harold <Hemlockdevelopment@gmail.com>
2017-05-02 10:50:07 +0200

    Part one

---
+++ public_html/js/exam4.js
@@ -0,0 +1,35 @@
+var app4 = angular.module('app4', []);
+ 
+// Define the Controller and implement the Scope
+app4.controller('eventCtrl', function($scope) {
+    $scope.blur = 0;
+    $scope.click = 0;
+    $scope.dblclick = 0;
+    $scope.copy = 0;
+    $scope.paste = 0;
+    $scope.cut = 0;
+    $scope.focus = 0;
+    $scope.change = 0;
+  
+    $scope.keydown = function(e) {
+    // Works for the basic characters and numbers
+        $scope.kdKey = String.fromCharCode(e.keyCode);
+    };
+  
+    $scope.mouseenter = 0;
+    $scope.mouseleave = 0;
+    
+    $scope.disable = true;
+    
+    $scope.dayTimeButton = true;
+    $scope.capitals = [
+      {"City": "Montgomery",
+      "State": "Alabama"},
+      {"City": "Juneau",
+      "State": "Alaska"},
+      {"City": "Phoenix",
+      "State": "Arizona"},
+      {"City": "Little Rock",
+      "State": "Arkansas"}
+    ];
+});
